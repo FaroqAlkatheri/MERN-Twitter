8f29604cae1a0abf5d8ae35db5d254fb28a898ae
FaroqAlkatheri <s201949170@kfupm.edu.sa>
2024-09-27 17:46:23 +0300

    signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,69 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.json({
-        data: "You hit the signup endpoint"
-    });
+    try{
+        const {username, fullName, email, password} = req.body;
+
+        const emailRegix = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailRegix.test(email)) {
+            return res.status(400).json({
+                message: "Invalid email format",
+            });
+        }
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists",
+            });
+        }
+
+        const existingEmail = await User.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).json({
+                message: "Email already exists",
+            });
+        }
+
+        //hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        //create new user
+        const newUser = new User({
+            username,
+            fullName,
+            email,
+            password: hashedPassword,
+        });
+
+        if (newUser){
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg
+            })
+        }
+        else {
+            res.status(400).json({
+                message: "Invalid user data",
+            });
+        }
+
+    }
+    catch (error) {
+        console.log("Error in signup: ", error);
+        res.status(500).json({
+            message: "Server error",
+        });
+    }
 };
 
 export const login = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js"
 import dotenv from "dotenv";
-import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // This parses incoming requests with JSON payloads and is based on body-parser.
 
 
 app.use("/api/auth", authRoutes);
